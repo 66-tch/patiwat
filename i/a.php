fdf6bcd8bd0fbf8e998f45b95b560c85b3ee5420
66-tch <66010914108@msu.ac.th>
2026-02-10 09:46:58 +0700

    Add files via upload

---
+++ i/a.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>ปฏิวัติ มาตวัน (มายมิ้น)</title>
+</head>
+
+<body>
+    <h1>งาน i</h1>
+    <h1>ปฏิวัติ มาตวัน (มายมิ้น)</h1>
+</body>
+</html>
