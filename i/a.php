fa381afd536b2dd752890ecac6e9576c18f3b994
66-tch <66010914108@msu.ac.th>
2026-02-10 10:46:06 +0700

    Add files via upload

--- i/a.php
+++ i/a.php
@@ -8,5 +8,24 @@
 <body>
     <h1>งาน i</h1>
     <h1>ปฏิวัติ มาตวัน (มายมิ้น)</h1>
+<table border="1">
+    <tr>
+        <th>รหัสภาค</th>
+        <th>ชื่อภาค</th>
+    </tr>
+
+<?php
+    include_once("connectdb.php");
+    $sql = "SELECT * FROM `regions` ORDER BY `regions`.`r_id` ASC";
+    $rs = mysqli_query($conn, $sql);
+
+    while($data = mysqli_fetch_array($rs)){
+?>
+    <tr>
+        <th><?php echo $data['r_id'];?></th>
+        <th><?php echo $data['r_name'];?></th>
+    </tr>
+<?php } ?>
+</table>
 </body>
 </html>
